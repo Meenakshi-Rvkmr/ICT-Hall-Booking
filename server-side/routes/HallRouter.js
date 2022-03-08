120d8f852cfe6766763bb88a09a57828209950d2
shemy <shemysb@gmail.com>
2022-03-08 18:34:16 +0530

    More Routes Added

--- server-side/routes/HallRouter.js
+++ server-side/routes/HallRouter.js
@@ -64,19 +64,45 @@ hallsRouter.get("/:id", async (req, res) => {
 
 //GET ALL HALL
 hallsRouter.get("/", async (req, res) => {
-    const halltype = req.query.hall;
-    try {
-      let halls;
-      if (halltype) {
-        halls = await Halls.find({ halltype }).sort({createdAt:-1}).limit(4);
-      } else {
-        halls = await Halls.find().sort({createdAt:-1}).limit(3);
-      }
-      res.status(200).json(halls);
-    } catch (err) {
-      res.status(500).json(err);
+  try {
+    let halls  = await Halls.find();
+    res.status(200).json(halls);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL HALL OFFESET
+hallsRouter.get("/:offsetValue/page", async (req, res) => {
+  const halltype = req.query.hall;
+  const tempValue=parseInt(req.params.offsetValue);
+  try {
+    let halls;
+    if (halltype) {
+      halls = await Halls.find({ halltype }).sort({createdAt:-1}).limit(3);
+    } else {
+      halls= await Halls.aggregate([
+        {$sort:{"createdAt":-1}},
+        {$skip:tempValue},
+        {$limit:3}
+      ])
     }
-  });
+    res.status(200).json(halls);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET TOTAL COUNT OF  HALL 
+hallsRouter.get("/counthall/all", async (req, res) => {
+  try {
+    let no_of_halls  = await Halls.countDocuments();
+    res.status(200).json(no_of_halls);
+  } catch (err) {
+    res.status(500).json(err);    
+  }
+});
+
 
 module.exports = hallsRouter;
 
